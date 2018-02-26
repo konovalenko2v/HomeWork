61a1a6a9737f1fdcdb64343dcad6e4e148b90285
konovalenko2v <konovalenko.2v@gmail.com>
2018-02-26 02:40:48 +0200

    Cicle0.1

---
+++ src/Cicle/Cicle.java
@@ -0,0 +1,10 @@
+package Cicle;
+
+public class Cicle {
+
+    public void cicle() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println("HI!");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Calculator.Calculator;
+import Cicle.Cicle;
 import Circle.Circle;
 
 import java.io.BufferedReader;
@@ -8,29 +9,16 @@ import java.io.InputStreamReader;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Какую программу запустить? Радиус круга = 1; Калькулятор = 2");
+        System.out.println("Какую программу запустить? Радиус круга = 1; Калькулятор = 2; Cicle = 3");
         String st = reader.readLine();
-        byte var4 = -1;
-        switch (st.hashCode()) {
-            case 49:
-                if (st.equals("1")) {
-                    var4 = 0;
-                }
-                break;
-            case 50:
-                if (st.equals("2")) {
-                    var4 = 1;
-                }
+        if (st == "1"){
+            new Circle();
         }
-
-        switch(var4) {
-            case 0:
-                new Circle();
-            case 1:
-                new Calculator();
-            default:
-                System.out.println("ты условие читать умеешь?");
-                System.out.println("Программа завершена без ошибок");
-        }
+        else if (st == "2"){
+            new Calculator();
+        System.out.println("есть че?");}
+        else if (st == "3"){
+            new Cicle();}
+        System.out.println("есть че?");
     }
 }
